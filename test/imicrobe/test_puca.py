a70b3eaedc8713b0797a1a6a92c57a906ac161e3
Joshua Lynch <jklynch@email.arizona.edu>
2018-01-25 16:49:20 -0500

    extract pieces needed for all tests

--- test/imicrobe/test_puca.py
+++ test/imicrobe/test_puca.py
@@ -1,57 +1,33 @@
 import json
-import os
-import sys
-import time
 
-from requests.auth import HTTPBasicAuth
-from oauthlib.oauth2 import BackendApplicationClient
-from requests_oauthlib import OAuth2Session
+import pytest
+
+from test import wait_for_job_to_finish
 
 
-def test_puca():
-    # grab client_id and client_secret:
-    client_id, client_secret = os.environ['AGAVE_CREDENTIALS'].split(':')
-    token_url = 'https://agave.iplantc.org/token'
-
-    # generate HTTPBasicAuth Header
-    basic_auth = HTTPBasicAuth(client_id, client_secret)
-    client = BackendApplicationClient(client_id=client_id)
-
-    # start oauth session
-    oauth = OAuth2Session(client=client)
-    token = oauth.fetch_token(token_url=token_url, auth=basic_auth)
-
+def test_puca_private(cyverse_oauth_session):
     # submit a job
     job_json = json.loads("""\
-    {
-      "name": "ohana-blast-test",
-      "appId": "ohana-blast-0.0.8",
-      "archive": true,
-      "inputs": {
-        "QUERY": "agave://data.iplantcollaborative.org/jklynch/data/muscope/blast/test.fa"
-      },
-      "parameter": {
-      }
-    }
+        {
+          "name": "imicrobe-puca-test",
+          "appId": "imicrobe-puca-0.0.2",
+          "archive": true,
+          "inputs": {
+            "inputDir": "test/imicrobe-puca"
+          },
+          "parameters": {
+            "BC_NAME": "samtools_1.6--0.img",
+            "CMD_LINE": "samtools quickcheck -v imicrobe-puca/test.bam"
+          }
+        }
     """)
 
-    submit_job_response = oauth.post(url='https://agave.iplantc.org/jobs/v2?pretty=true', json=job_json)
+    submit_job_response = cyverse_oauth_session.post(
+        url='https://agave.iplantc.org/jobs/v2?pretty=true', json=job_json)
     print(submit_job_response.json())
 
-    # ask for status on the job
-    job_id = submit_job_response.json()['result']['id']
-    job_status_url = 'https://agave.iplantc.org/jobs/v2/{}/status'.format(job_id)
-
-    job_status = None
-    while not (job_status == 'FINISHED' or job_status == 'FAILED'):
-        job_status_response = oauth.get(url=job_status_url)
-        job_status = job_status_response.json()['result']['status']
-
-        print(job_status)
-
-        sys.stdout.write('sleeping 60 seconds')
-        for j in range(6):
-            sys.stdout.write('.')
-            time.sleep(10)
+    job_status = wait_for_job_to_finish(
+        job_id=submit_job_response.json()['result']['id'],
+        cyverse_oauth_session=cyverse_oauth_session)
 
     assert job_status == 'FINISHED'
