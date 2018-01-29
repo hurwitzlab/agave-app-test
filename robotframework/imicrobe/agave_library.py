87cf02d64c0166fef082f9da6074fdd17e453eac
Joshua Lynch <jklynch@email.arizona.edu>
2018-01-29 13:43:44 -0500

    add oath login and job polling

---
+++ robotframework/imicrobe/agave_library.py
@@ -0,0 +1,59 @@
+import json
+import os
+import sys
+import time
+
+import requests.auth
+import oauthlib.oauth2
+import requests_oauthlib
+
+
+class agave_library():
+
+    def __init__(self, agave_base_url):
+        self._agave_base_url = agave_base_url
+        self._oauth = None
+        self._job_response = None
+
+
+    def oauth_login(self):
+        client_id, client_secret = os.environ['AGAVE_CREDENTIALS'].split(':')
+        token_url = '{}/token'.format(self._agave_base_url)
+
+        # generate HTTPBasicAuth Header
+        basic_auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
+        client = oauthlib.oauth2.BackendApplicationClient(client_id=client_id)
+
+        # start oauth session
+        self._oauth = requests_oauthlib.OAuth2Session(client=client)
+        self._oauth.fetch_token(token_url=token_url, auth=basic_auth)
+
+        return self._oauth
+
+
+    def submit_job(self, job_json):
+        self._job_response = self._oauth.post(
+            url='{}/jobs/v2?pretty=true'.format(self._agave_base_url),
+            json=job_json)
+
+        print(self._job_response)
+        return self._job_response
+
+
+    def wait_for_job_to_finish(self, job_id):
+        job_status_url = '{}/jobs/v2/{}/status'.format(self._agave_base_url, job_id)
+
+        job_status_response = self._oauth.get(url=job_status_url)
+        job_status = job_status_response.json()['result']['status']
+        print(job_status)
+        while not (job_status == 'FINISHED' or job_status == 'FAILED'):
+            sys.stdout.write('sleeping 60 seconds')
+            for j in range(6):
+                sys.stdout.write('.')
+                time.sleep(10)
+            job_status_response = self._oauth.get(url=job_status_url)
+            job_status = job_status_response.json()['result']['status']
+
+            print(job_status)
+
+        return job_status
--- robotframework/imicrobe/resource.robot
+++ robotframework/imicrobe/resource.robot
@@ -3,8 +3,8 @@ Documentation	Blah blah blah
 Library		RequestsLibrary
 Library		Collections
 Library		JSONLibrary
+Library     robotframework.imicrobe.agave_library  https://agave.iplantc.org
 
 *** Variables ***
-${AUTH_TOKEN}       	4fe19b3ec14c5e388484ed1b94618a
 ${AGAVE_BASE_URL}	https://agave.iplantc.org
 ${JOBS}			/jobs/v2
